d71217e9d868a35dd1bba816b9f0f26ac3ad8ade
Ali Sheikh <l_lawliet786@hotmail.com>
2026-07-19 15:30:18 -0400

    find all users functionality

--- backend/src/main/java/com/project/Bysell/service/UserService.java
+++ backend/src/main/java/com/project/Bysell/service/UserService.java
@@ -3,7 +3,11 @@ package com.project.Bysell.service;
 import com.project.Bysell.model.User;
 import com.project.Bysell.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -17,5 +21,14 @@ public class UserService {
 
     public User createUser(User user) {
         return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with id: " + id));
     }
 }
